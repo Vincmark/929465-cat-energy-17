aae9e93adcd8b9dc5d8bb627e92c7bcc91a20b5d
Vincmark <44532137+Vincmark@users.noreply.github.com>
2019-07-31 23:31:32 +0300

    Мобильная версия проекта

--- source/js/script.js
+++ source/js/script.js
@@ -20,7 +20,7 @@ if (menuToggleButton!==null) {
 var catToggleLinkBefore = document.querySelector('.live-example__cat-btn--before');
 var catToggleLinkAfter = document.querySelector('.live-example__cat-btn--after');
 var catImageHolder = document.querySelector('.live-example__cat');
-var catToggleCheckbox = document.querySelector('.live-example__slider-central');
+var catToggleSlider = document.querySelector('.live-example__slider-central');
 
 if (catToggleLinkBefore!==null) {
     catToggleLinkBefore.addEventListener('click', function (evt) {
@@ -28,6 +28,7 @@ if (catToggleLinkBefore!==null) {
         if (catImageHolder.classList.contains('live-example__cat--after')) {
             catImageHolder.classList.remove('live-example__cat--after');
             catImageHolder.classList.add('live-example__cat--before');
+            catToggleSlider.value = '1';
         }
     });
 }
@@ -38,23 +39,105 @@ if (catToggleLinkAfter!==null) {
         if (catImageHolder.classList.contains('live-example__cat--before')) {
             catImageHolder.classList.remove('live-example__cat--before');
             catImageHolder.classList.add('live-example__cat--after');
+            catToggleSlider.value = '2';
         }
     });
 }
 
-if (catToggleCheckbox!==null) {
-    catToggleCheckbox.addEventListener('CheckboxStateChange', function (evt) {
+if (catToggleSlider!==null) {
+    catToggleSlider.addEventListener('click', function (evt) {
         evt.preventDefault();
-        if (catToggleCheckbox.checked) {
-            alert('checked');
-            // catImageHolder.classList.remove('live-example__cat--before');
-            // catImageHolder.classList.add('live-example__cat--after');
-            // catToggleCheckbox.checked = false;
-        } else {
-            alert('unchecked');
-            // catImageHolder.classList.remove('live-example__cat--after');
-            // catImageHolder.classList.add('live-example__cat--before');
-            // catToggleCheckbox.checked = true;
+        console.log('slider alive');
+        // console.log(catToggleSlider.value);
+        if (catToggleSlider.value === '1') {
+            if (catImageHolder.classList.contains('live-example__cat--after')) {
+                catImageHolder.classList.remove('live-example__cat--after');
+                catImageHolder.classList.add('live-example__cat--before');
+            }
+        } else if (catToggleSlider.value === '2') {
+            if (catImageHolder.classList.contains('live-example__cat--before')) {
+                catImageHolder.classList.remove('live-example__cat--before');
+                catImageHolder.classList.add('live-example__cat--after');
+            }
         }
     });
 }
+
+
+// Form validation
+var submitButton = document.querySelector('.form__submit');
+var form = document.querySelector('.form');
+
+var catName = document.getElementById('cat-name');
+var weight = document.getElementById('weight');
+var email = document.getElementById('email');
+var phone = document.getElementById('phone');
+
+
+if (submitButton!==null) {
+    submitButton.addEventListener('click', function (evt) {
+        evt.preventDefault();
+
+        var validation = true;
+        var errorMsg = 'Проверьте правильность заполнения:\n';
+
+        // Name
+        var catNameStr = catName.value;
+        catNameStr.trim();
+        catName.classList.remove('form__input-text--error');
+        if (catNameStr.length === 0) {
+            validation = false;
+            catName.classList.add('form__input-text--error');
+            errorMsg += '- Имя кота должно быть заполнено\n';
+
+        }
+
+        // Weight
+        weight.classList.remove('form__input-text--error');
+        var catWeight = weight.value;
+        catWeight.trim();
+        if ((catWeight.length === 0 ) || (isNaN(Number(catWeight)))) {
+            validation = false;
+            weight.classList.add('form__input-text--error');
+            errorMsg += '- Вес кота должен быть заполнен\n';
+
+        }
+
+        // Email
+        var emailStr = email.value;
+        emailStr.trim();
+        email.classList.remove('form__input-email--error');
+        if ((emailStr.length === 0) || (!isEmail(emailStr))) {
+            validation = false;
+            email.classList.add('form__input-email--error');
+            errorMsg += '- Емейл должен быть правильно заполнен\n';
+        }
+
+
+        //Phone
+        var phoneStr = phone.value;
+        phoneStr.trim();
+        phone.classList.remove('form__input-tel--error');
+        if ((phoneStr.length === 0)||(!isPhone(phoneStr))) {
+            validation = false;
+            phone.classList.add('form__input-tel--error');
+            errorMsg += '- Телефон должен быть правильно заполнен\n';
+        }
+
+        if (validation) {
+            //form.submit();
+        } else {
+            alert(errorMsg);
+        }
+    });
+}
+
+var isEmail = function(eml) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(eml);
+};
+
+var isPhone = function(tel) {
+    var re = /((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
+    return re.test(tel);
+};
